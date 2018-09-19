6c228da36fa6d375e96ea2571bb7ecd4ad499d50
liuchunhua1 <chunhua1@eastmoney.com>
2018-09-19 15:01:11 +0800

    把某个model 放到admin中进行管理

--- appA/admin.py
+++ appA/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import Profile
-# Register your models here.
 
-admin.register(Profile)
+admin.site.site_header = '登录'
+
+admin.site.site_title = '用户登录title'
+
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('name', 'picture')
--- appA/models.py
+++ appA/models.py
@@ -4,12 +4,14 @@ from django.utils import timezone
 # Create your models here.
 # 用来保存上传图片相关信息的模型
 class Profile(models.Model):
-    name = models.CharField(max_length = 50)
+    name = models.CharField(max_length = 50,verbose_name='名称')
     # upload_to 表示图像保存路径
-    picture = models.ImageField(upload_to = 'test_pictures')
+    picture = models.ImageField(upload_to = 'test_pictures',verbose_name='上传图片')
 
     class Meta:
         db_table = "profile"
+        verbose_name = "图片"
+        verbose_name_plural = "图片管理"
 
     def __str__(self):
         return self.name
