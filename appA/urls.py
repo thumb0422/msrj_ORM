07da823d2b7e821e46fbe48926379fd2ceac29ca
liuchunhua <thumb0422@163.com>
2018-09-19 22:14:00 +0800

    add bpptstrap

--- appA/urls.py
+++ appA/urls.py
@@ -5,7 +5,7 @@ from .views import save_photo,index_form,index
 urlpatterns = [
     url(r'^$', index, name='index'),
     # 上传并保存图片的 url
-    url(r'^save_photo/', save_photo, name='save_profile'),
+    url(r'^save_photo/', save_photo, name='save_photo'),
     # 展示表单的 url
     url(r'^index_form/', index_form, name='index_form'),
 ]
