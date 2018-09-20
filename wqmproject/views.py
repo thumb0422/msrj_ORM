e52547ae65989f52bb5372e840ae55769505c8de
liuchunhua1 <chunhua1@eastmoney.com>
2018-09-20 16:09:30 +0800

    增加 40X页面，然而有问题

---
+++ wqmproject/views.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render
+
+def page_not_found(request):
+    return render(request, '404.html')
+
+
+def page_error(request):
+    return render(request, '500.html')
+
+
+def permission_denied(request):
+    return render(request, '403.html')
+
+
+def bad_request(request):
+    return render(request, '400.html')
