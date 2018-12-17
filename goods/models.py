044f526737fc603cca1659d07381c0e159593333
liuchunhua1 <chunhua1@eastmoney.com>
2018-12-17 14:25:42 +0800

    update

---
+++ goods/models.py
@@ -0,0 +1,24 @@
+from django.db import models
+from django.utils import timezone
+from utility.strExtension import generate_random_str
+
+# Create your models here.
+class ProductInfo(models.Model):
+    id = models.IntegerField(primary_key=True)
+    code = models.CharField(max_length=20,unique=True,default=generate_random_str(6))
+    name = models.CharField(max_length=50,null=False)
+    costPrice = models.DecimalField(max_digits=10,decimal_places=2)
+    unit = models.CharField(max_length=10)
+    salePrice = models.DecimalField(max_digits=10,decimal_places=2)
+
+    isValid = models.BooleanField(default=True)
+    create_Date = models.DateTimeField(default=timezone.now)
+    update_Date = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'product_info'
+        verbose_name = '产品信息'
+        verbose_name_plural = '产品信息'
+
+    def __str__(self):
+        return self.name
--- wqmproject/settings.py
+++ wqmproject/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     # 'appA',
     'appAPI',
     'app001',
+    'goods',
 ]
 
 MIDDLEWARE = [
