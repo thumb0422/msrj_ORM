2cc14abf32aac73bcd319daa59bfc38128555119
liuchunhua1 <chunhua1@eastmoney.com>
2019-01-08 17:34:26 +0800

    测试 装饰器

--- trade/tests.py
+++ trade/tests.py
@@ -1,3 +1,27 @@
-from django.test import TestCase
 
-# Create your tests here.
+import time
+
+def myDecorate2(func):
+    def aaaTime(*args,**kwargs):
+        print('aaaTime -----1')
+        func(*args,**kwargs)
+        print('aaaTime -----2')
+    return aaaTime
+
+
+def myDecorate1(func):
+    def countTime(*args,**kwargs):
+        start = time.time()
+        func(*args,**kwargs)
+        end = time.time()
+        print('spent time = %s'%(end-start))
+    return countTime
+
+@myDecorate2
+@myDecorate1
+def sumAB(a,b):
+    print(a*b)
+    return a*b
+
+
+sumAB(10,20)
